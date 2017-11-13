5f985d65af5996cbd65e6406321e382ceecd4327
Henri Bergius <henri.bergius@iki.fi>
2017-11-13 15:28:46 +0100

    Remove Grunt and start ES6 port

---
+++ spec/API.js
@@ -0,0 +1,18 @@
+const canadianness = require('../index.js');
+const chai = require('chai');
+
+describe('Javascript API', () =>
+
+  describe('calling with sad content', () => {
+    const content = 'life is hard and then you die, ...eh';
+    it('should return emotion=sadness', done =>
+      canadianness(content, {}, (err, result) => {
+        chai.expect(err).to.not.exist;
+        chai.expect(result).to.include.keys(['emotion', 'score']);
+        chai.expect(result.emotion).to.equal('sadness');
+        chai.expect(result.score).to.be.above(5);
+        done();
+      })
+    );
+  })
+);
