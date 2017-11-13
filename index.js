ccdb5a132958135b847495acfa5897adf572b5be
Henri Bergius <henri.bergius@iki.fi>
2017-11-13 17:26:08 +0100

    Simplify index

--- index.js
+++ index.js
@@ -6,44 +6,28 @@ const defaultWords = {
   'eh!': 11,
 };
 
-const canadianness = (contentData, options, callback) => {
+// Produce the JavaScript entry point
+function canadianness(contentData, options, callback) {
+  // Normalize options
   const spellingData = options.spelling || defaultSpellingData;
   const wordsData = options.words || defaultWords;
-  const componentName = 'canadianness/Canadianness';
+
+  // Convert options and input to a set of NoFlo packets to be sent
   const inputs = {
     words: wordsData,
     spelling: spellingData,
     content: contentData,
   };
 
+  // Produce a NoFlo.asCallback wrapped function to execute our graph
+  const componentName = 'canadianness/Canadianness';
   const wrapperFunction = noflo.asCallback(componentName, {
     baseDir: __dirname,
   });
-  return wrapperFunction(inputs, callback);
-};
+
+  // Run the graph with inputs and call callback
+  wrapperFunction(inputs, callback);
+}
 
 // Expose function as public API
 module.exports = canadianness;
-
-// ## Command-line program
-function main() {
-  const content = process.argv[2];
-
-  const options = {
-    spelling: null,
-    words: null,
-  };
-
-  canadianness(content, options, (err, results) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(results.score, results.emotion);
-  });
-}
-
-// Only run main if we are not imported as a module
-if (!module.parent) {
-  main();
-}
